d7a9d93ab602aa23e2a358af31cb1d7470466914
pierreyoda <pierreyoda@users.noreply.github.com>
2018-03-02 20:57:53 +0100

    Added unit tests to activation module

--- src/activation.rs
+++ src/activation.rs
@@ -37,3 +37,59 @@ impl<F: NdFloat> Activation<F> for Rectifier {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::Float;
+    use super::*;
+
+    fn test_numerical_function<A>(function: A, inputs: Vec<f64>, outputs: Vec<f64>)
+    where
+        A: Activation<Float>,
+    {
+        assert_eq!(inputs.len(), outputs.len());
+        for i in 0..inputs.len() {
+            let result = function.compute(inputs[i]);
+            assert_relative_eq!(result, outputs[i]);
+        }
+    }
+
+    #[test]
+    fn sigmoid() {
+        test_numerical_function(
+            Sigmoid,
+            vec![-2.0, -1.0, 0.0, 1.0, 2.0],
+            vec![
+                0.1192029220221175,
+                0.2689414213699951,
+                0.5,
+                0.7310585786300048,
+                0.8807970779778824,
+            ],
+        );
+    }
+
+    #[test]
+    fn tanh() {
+        test_numerical_function(
+            TanH,
+            vec![-1.0, -0.5, 0.0, 0.5, 1.0],
+            vec![
+                -0.7615941559557649,
+                -0.4621171572600097,
+                0.0,
+                0.4621171572600097,
+                0.7615941559557649,
+            ],
+        );
+    }
+
+    #[test]
+    fn relu() {
+        test_numerical_function(
+            Rectifier,
+            vec![-150.0, -7.0, 0.0, 3.0, 10.0],
+            vec![0.0, 0.0, 0.0, 3.0, 10.0],
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+#[macro_use]
+extern crate approx;
 extern crate ndarray;
 extern crate rand;
 
