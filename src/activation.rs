b6db7b63755a1bf52a91e5d069890ba64074476e
pierreyoda <pierreyoda@users.noreply.github.com>
2018-03-02 17:09:29 +0100

    Added the TanH and ReLU activation functions

--- src/activation.rs
+++ src/activation.rs
@@ -6,9 +6,26 @@ pub trait Activation<F: NdFloat> {
     fn compute(&self, x: F) -> F;
 }
 
+/// The Sigmoid function squashes a real value into the ]0, 1[ range.
 pub struct Sigmoid;
 impl<F: NdFloat> Activation<F> for Sigmoid {
     fn compute(&self, x: F) -> F {
         F::one() / (F::one() + (-x).exp())
+    }
+}
+
+/// The Hyperbolic tangent squashes a real value into the ]-1, 1[ range.
+pub struct TanH;
+impl<F: NdFloat> Activation<F> for TanH {
+    fn compute(&self, x: F) -> F {
+        x.tanh()
+    }
+}
+
+/// The Rectified Linear Unit (ReLU) functions replaces negative values with 0.
+pub struct Rectifier;
+impl<F: NdFloat> Activation<F> for Rectifier {
+    fn compute(&self, x: F) -> F {
+        if x < F::zero() { F::zero() } else { x }
     }
 }
