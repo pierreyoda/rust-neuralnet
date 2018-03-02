ce7e9c41e0ce05d04f63d468dfbae0a021198de5
pierreyoda <pierreyoda@users.noreply.github.com>
2018-03-02 23:10:22 +0100

    Refactored the activation module to work on vectors

--- src/activation.rs
+++ src/activation.rs
@@ -1,25 +1,25 @@
-use ndarray::NdFloat;
+use ndarray::{Array1, NdFloat};
 
 /// An activation function in a Neural Network defines whether a neuron will
 /// send a signal to its outputs or not.
 pub trait Activation<F: NdFloat> {
     #[inline]
-    fn compute(&self, x: F) -> F;
+    fn compute(&self, x: &Array1<F>) -> Array1<F>;
 
     #[inline]
-    fn compute_derivative(&self, x: F) -> F;
+    fn compute_derivative(&self, x: &Array1<F>) -> Array1<F>;
 }
 
 /// The Identity function.
 pub struct Identity;
 impl<F: NdFloat> Activation<F> for Identity {
     #[inline]
-    fn compute(&self, x: F) -> F {
-        x
+    fn compute(&self, x: &Array1<F>) -> Array1<F> {
+        x.clone()
     }
     #[inline]
-    fn compute_derivative(&self, _: F) -> F {
-        F::one()
+    fn compute_derivative(&self, x: &Array1<F>) -> Array1<F> {
+        Array1::from_vec(vec![F::one(); x.dim()])
     }
 }
 
@@ -27,13 +27,15 @@ impl<F: NdFloat> Activation<F> for Identity {
 pub struct Sigmoid;
 impl<F: NdFloat> Activation<F> for Sigmoid {
     #[inline]
-    fn compute(&self, x: F) -> F {
-        F::one() / (F::one() + (-x).exp())
+    fn compute(&self, x: &Array1<F>) -> Array1<F> {
+        x.map(|v: &F| F::one() / (F::one() + (-*v).exp()))
     }
     #[inline]
-    fn compute_derivative(&self, x: F) -> F {
-        let y = self.compute(x);
-        y * (F::one() - y)
+    fn compute_derivative(&self, x: &Array1<F>) -> Array1<F> {
+        x.map(|v: &F| {
+            let y = F::one() / (F::one() + (-*v).exp());
+            y * (F::one() - y)
+        })
     }
 }
 
@@ -41,13 +43,12 @@ impl<F: NdFloat> Activation<F> for Sigmoid {
 pub struct TanH;
 impl<F: NdFloat> Activation<F> for TanH {
     #[inline]
-    fn compute(&self, x: F) -> F {
-        x.tanh()
+    fn compute(&self, x: &Array1<F>) -> Array1<F> {
+        x.map(|v| v.tanh())
     }
     #[inline]
-    fn compute_derivative(&self, x: F) -> F {
-        let y = self.compute(x);
-        F::one() - y.powi(2)
+    fn compute_derivative(&self, x: &Array1<F>) -> Array1<F> {
+        x.map(|v| F::one() - v.tanh().powi(2))
     }
 }
 
@@ -55,20 +56,14 @@ impl<F: NdFloat> Activation<F> for TanH {
 pub struct Rectifier;
 impl<F: NdFloat> Activation<F> for Rectifier {
     #[inline]
-    fn compute(&self, x: F) -> F {
-        if x < F::zero() {
-            F::zero()
-        } else {
-            x
-        }
+    fn compute(&self, x: &Array1<F>) -> Array1<F> {
+        let zero = F::zero();
+        x.map(|v| if *v < zero { zero } else { *v })
     }
     #[inline]
-    fn compute_derivative(&self, x: F) -> F {
-        if x < F::zero() {
-            F::zero()
-        } else {
-            F::one()
-        }
+    fn compute_derivative(&self, x: &Array1<F>) -> Array1<F> {
+        let (zero, one) = (F::zero(), F::one());
+        x.map(|v| if *v < zero { zero } else { one })
     }
 }
 
@@ -79,42 +74,46 @@ mod tests {
 
     fn test_numerical_function<A>(
         function: A,
-        inputs: Vec<f64>,
-        values: Vec<f64>,
-        derivatives: Vec<f64>,
+        inputs: Array1<Float>,
+        values: Array1<Float>,
+        derivatives: Array1<Float>,
     ) where
         A: Activation<Float>,
     {
         assert_eq!(inputs.len(), values.len());
         assert_eq!(inputs.len(), derivatives.len());
+        let computed_values = function.compute(&inputs);
+        let computed_derivatives = function.compute_derivative(&inputs);
         for i in 0..inputs.len() {
-            let input = inputs[i];
-            let value = function.compute(input);
-            let derivative = function.compute_derivative(input);
-            assert_relative_eq!(value, values[i]); // 16 digits precision by default
-            assert_relative_eq!(derivative, derivatives[i]);
+            assert_relative_eq!(computed_values[i], values[i]); // 16 digits precision by default
+            assert_relative_eq!(computed_derivatives[i], derivatives[i]);
         }
     }
 
     #[test]
     fn identity() {
-        let inputs = vec![-23.0, -7.0, 0.0, 3.0, 10.0];
-        test_numerical_function(Identity, inputs.clone(), inputs, vec![1.0; 5]);
+        let inputs = array![-23.0, -7.0, 0.0, 3.0, 10.0];
+        test_numerical_function(
+            Identity,
+            inputs.clone(),
+            inputs,
+            Array1::from_vec(vec![1.0; 5]),
+        );
     }
 
     #[test]
     fn sigmoid() {
         test_numerical_function(
             Sigmoid,
-            vec![-2.0, -1.0, 0.0, 1.0, 2.0],
-            vec![
+            array![-2.0, -1.0, 0.0, 1.0, 2.0],
+            array![
                 0.1192029220221175,
                 0.2689414213699951,
                 0.5,
                 0.7310585786300048,
                 0.8807970779778824,
             ],
-            vec![
+            array![
                 0.1049935854035065,
                 0.1966119332414819,
                 0.25,
@@ -128,15 +127,15 @@ mod tests {
     fn tanh() {
         test_numerical_function(
             TanH,
-            vec![-1.0, -0.5, 0.0, 0.5, 1.0],
-            vec![
+            array![-1.0, -0.5, 0.0, 0.5, 1.0],
+            array![
                 -0.7615941559557649,
                 -0.4621171572600097,
                 0.0,
                 0.4621171572600097,
                 0.7615941559557649,
             ],
-            vec![
+            array![
                 0.4199743416140261,
                 0.7864477329659274,
                 1.0,
@@ -150,9 +149,9 @@ mod tests {
     fn relu() {
         test_numerical_function(
             Rectifier,
-            vec![-150.0, -7.0, 0.0, 3.0, 10.0],
-            vec![0.0, 0.0, 0.0, 3.0, 10.0],
-            vec![0.0, 0.0, 1.0, 1.0, 1.0],
+            array![-150.0, -7.0, 0.0, 3.0, 10.0],
+            array![0.0, 0.0, 0.0, 3.0, 10.0],
+            array![0.0, 0.0, 1.0, 1.0, 1.0],
         );
     }
 }
