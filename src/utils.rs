e86cc547ac1b195e5e1a5708923a83c85c2be309
pierreyoda <pierreyoda@users.noreply.github.com>
2018-04-01 20:06:21 +0200

    Added vectors concatenation function to the utils module

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 use rand::distributions::IndependentSample;
-use ndarray::{Array1, Array2, ArrayBase, DataOwned, Dimension, NdFloat, ShapeBuilder};
+use ndarray::{Array1, Array2, ArrayBase, DataOwned, Dimension, NdFloat, ShapeBuilder, Zip};
 
 /// Extends ndarray's ```ArrayBase``` to provide random arrays of any shape
 /// and any data type.
@@ -51,10 +51,33 @@ pub fn flatten_array<F: NdFloat>(array: Array2<F>) -> Option<Array1<F>> {
     }
 }
 
+/// Concatenates two ndarray vectors into a single one.
+/// TODO: there is probably a better, more idiomatic way
+pub fn concat_vectors<F: NdFloat>(left: &Array1<F>, right: &Array1<F>) -> Array1<F> {
+    let left_len = left.len();
+    let mut concat: Array1<F> = Array1::zeros(left_len + right.len());
+    Zip::indexed(&mut concat).apply(|i, v| {
+        *v = if i < left_len {
+            *left.get(i).unwrap()
+        } else {
+            *right.get(i - left_len).unwrap()
+        };
+    });
+    concat
+}
+
 #[cfg(test)]
 mod tests {
-    use ndarray::{Array2, arr2};
-    use super::flatten_array;
+    use ndarray::{Array1, Array2, arr1, arr2};
+    use super::{concat_vectors, flatten_array};
+    use super::super::Float;
+
+    fn test_vector(vector: &Array1<Float>, values: Vec<Float>) {
+        assert!(vector.len() >= values.len());
+        for i in 0..values.len() {
+            assert_relative_eq!(vector.get(i).unwrap(), &values[i]);
+        }
+    }
 
     #[test]
     fn flatten_array_function() {
@@ -62,11 +85,13 @@ mod tests {
         let maybe_array_flat = flatten_array(array);
         assert!(maybe_array_flat.is_some());
         let array_flat = maybe_array_flat.unwrap();
-        assert_relative_eq!(array_flat.get(0).unwrap(), &1.0);
-        assert_relative_eq!(array_flat.get(1).unwrap(), &2.0);
-        assert_relative_eq!(array_flat.get(2).unwrap(), &3.0);
-        assert_relative_eq!(array_flat.get(3).unwrap(), &4.0);
-        assert_relative_eq!(array_flat.get(4).unwrap(), &5.0);
-        assert_relative_eq!(array_flat.get(5).unwrap(), &6.0);
+        test_vector(&array_flat, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+    }
+
+    #[test]
+    fn concat_vectors_function() {
+        let (vector_left, vector_right) = (arr1(&[1.0, 2.0]), arr1(&[3.0, 4.0]));
+        let vector_concat = concat_vectors(&vector_left, &vector_right);
+        test_vector(&vector_concat, vec![1.0, 2.0, 3.0, 4.0]);
     }
 }
