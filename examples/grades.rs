93eec97cf17c78c5870a12bb4fe99238bdd9f43e
pierreyoda <pierreyoda@users.noreply.github.com>
2018-04-06 17:59:53 +0200

    Improved grades example's documentation

--- examples/grades.rs
+++ examples/grades.rs
@@ -1,6 +1,21 @@
-//! Given a ((hours of sleep), (hours of study) => (grade)) dataset,
-//! predict a grade for any ((hours of sleep), (hours of stury)) input tuple
+//! Given an ((hours of sleep), (hours of study) => (grade)) dataset,
+//! predict a grade for any ((hours of sleep), (hours of study)) input tuple
 //! using an Artificial Neural Network.
+//!
+//! Based on the Neural Network Demystified series by the Youtube channel Welch Lab.
+//!
+//! # Topology
+//!
+//! ## Inputs
+//!
+//! - hours of sleep : floating point scalar (>= 0)
+//!
+//! - hours of study : floating point scalar (>= 0)
+//!
+//! ## Output(s)
+//!
+//! test grade : floating point scalar (0 - 100)
+//!
 
 extern crate ndarray;
 extern crate rand;
@@ -11,7 +26,7 @@ use rand::thread_rng;
 
 use rust_neuralnet::activation::Sigmoid;
 use rust_neuralnet::builder::NeuralNetworkBuilder;
-use rust_neuralnet::training::{Sample, prepare_dataset};
+use rust_neuralnet::training::{prepare_dataset, Sample};
 
 fn main() {
     let mut rng = rand::thread_rng();
@@ -21,10 +36,9 @@ fn main() {
         Sample::dataset(vec![5.0, 1.0], vec![82.0]),
         Sample::dataset(vec![10.0, 2.0], vec![93.0]),
     ];
-    let (inputs, expected_outputs) = prepare_dataset(&dataset).unwrap();
+    let (inputs, expected_outputs) = prepare_dataset(&dataset).expect("dataset preparation error");
 
-    let mut neural_network = NeuralNetworkBuilder::with_inputs(2)
-        .output(3, 1, Sigmoid, &mut rng);
+    let mut neural_network = NeuralNetworkBuilder::with_inputs(2).output(3, 1, Sigmoid, &mut rng);
 
     {
         let outputs = neural_network.run_forward(inputs.view());
